1e00694c01567bc280335259afc73b462e7f101b
Gorane7 <kristjankv32@gmail.com>
2019-08-06 14:38:21 +0300

    Added a few elements to the visual interface

--- visual.py
+++ visual.py
@@ -4,8 +4,10 @@ from gorgame import game
 global my_game
 display = game.basics.Coords([1100, 600])
 top_bar = game.basics.Coords([display.x, 100])
-right_bar = game.basics.Coords([200, display.y + top_bar.y])
-size = (display.x + right_bar.x, display.y + top_bar.y)
+middle_bar = game.basics.Coords([200, display.y + top_bar.y])
+button = game.basics.Coords([middle_bar.x, 50])
+size = (display.x * 2 + middle_bar.x, display.y + top_bar.y)
+display_order = ["DM", "party"]
 
 
 def main():
@@ -16,8 +18,13 @@ def main():
 def setup():
     global my_game
     my_game = game.Game(size)
-    my_game.screen.window.add_component([0, 0], top_bar, "red", 1, "top bar", window = True)
-    my_game.screen.window.add_component([0, top_bar.y], display, "grey", 1, "dispay", spaceview = True)
-    my_game.screen.window.add_component([display.x, 0], right_bar, "blue", 1, "right bar", window = True)
+    my_game.screen.window.add_component([0, 0], top_bar, 1, display_order[0] + " bar", background = "red", window = True)
+    my_game.screen.window.add_component([0, top_bar.y], display, 1, display_order[0] + " display", background = "grey", spaceview = True)
+    my_game.screen.window.add_component([display.x, 0], middle_bar, 1, "middle bar", background = "blue", window = True)
+    my_game.screen.window.add_component([display.x + middle_bar.x, 0], top_bar, 1, display_order[1] + " bar", background = "red", window = True)
+    my_game.screen.window.add_component([display.x + middle_bar.x, top_bar.y], display, 1, display_order[1] + " display", background = "grey", spaceview = True)
+
+    my_game.screen.window.get("middle bar").add_component([0, 0], button, 1, "new map button", background = "green", button = True)
+    my_game.screen.window.get("middle bar").get("new map button").add_text("New map", "black")
 
 main()
