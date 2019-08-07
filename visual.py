9f508c7f9aae0f001cf3d3974cba7ef5e5a813df
Gorane7 <kristjankv32@gmail.com>
2019-08-07 14:01:11 +0300

    Added buttons that allow you to make, save and load a map with certain dimensions and tile size

--- visual.py
+++ visual.py
@@ -1,30 +1,158 @@
 from gorgame import game
+import os
 
 #CONSTANTS
 global my_game
 display = game.basics.Coords([1100, 600])
 top_bar = game.basics.Coords([display.x, 100])
 middle_bar = game.basics.Coords([200, display.y + top_bar.y])
-button = game.basics.Coords([middle_bar.x, 50])
+button = game.basics.Coords([middle_bar.x, 100])
 size = (display.x * 2 + middle_bar.x, display.y + top_bar.y)
 display_order = ["DM", "party"]
+current_map = None
 
 
 def main():
     setup()
     while True:
         my_game.loop()
+        my_loop()
+
+def my_loop():
+    check_new_map_button()
+    check_new_map_inputs()
+    check_save_map_button()
+    check_load_map_button()
+    check_load_map_inputs()
 
 def setup():
     global my_game
     my_game = game.Game(size)
     my_game.screen.window.add_component([0, 0], top_bar, 1, display_order[0] + " bar", background = "red", window = True)
-    my_game.screen.window.add_component([0, top_bar.y], display, 1, display_order[0] + " display", background = "grey", spaceview = True)
+    my_game.screen.window.add_component([0, top_bar.y], display, 0, display_order[0] + " grid", background = "grey", gridview = True)
+    my_game.screen.window.add_component([0, top_bar.y], display, 1, display_order[0] + " space", spaceview = True)
     my_game.screen.window.add_component([display.x, 0], middle_bar, 1, "middle bar", background = "blue", window = True)
     my_game.screen.window.add_component([display.x + middle_bar.x, 0], top_bar, 1, display_order[1] + " bar", background = "red", window = True)
-    my_game.screen.window.add_component([display.x + middle_bar.x, top_bar.y], display, 1, display_order[1] + " display", background = "grey", spaceview = True)
+    my_game.screen.window.add_component([display.x + middle_bar.x, top_bar.y], display, 0, display_order[1] + " grid", background = "grey", gridview = True)
+    my_game.screen.window.add_component([display.x + middle_bar.x, top_bar.y], display, 1, display_order[1] + " space", spaceview = True)
 
     my_game.screen.window.get("middle bar").add_component([0, 0], button, 1, "new map button", background = "green", button = True)
-    my_game.screen.window.get("middle bar").get("new map button").add_text("New map", "black")
+    my_game.screen.window.get("middle bar").get("new map button").change_attributes(text = "New map", colour = "black")
+    my_game.screen.window.get("middle bar").add_component([0, button.y], button, 1, "save map button", background = "green", button = True)
+    my_game.screen.window.get("middle bar").get("save map button").change_attributes(text = "Save map", colour = "black")
+    my_game.screen.window.get("middle bar").add_component([0, button.y * 2], button, 1, "load map button", background = "green", button = True)
+    my_game.screen.window.get("middle bar").get("load map button").change_attributes(text = "Load map", colour = "black")
+
+def check_new_map_button():
+    if my_game.screen.window.get("middle bar").get("new map button").pressed:
+        make_new_map_inputs()
+
+def check_save_map_button():
+    if my_game.screen.window.get("middle bar").get("save map button").pressed:
+        save_map()
+
+def check_load_map_button():
+    if my_game.screen.window.get("middle bar").get("load map button").pressed:
+        make_load_map_inputs()
+
+def save_map():
+    if not current_map:
+        return
+    x = len(my_game.maps[current_map + " map"].tiles)
+    y = len(my_game.maps[current_map + " map"].tiles[0])
+    tile = int(my_game.spaces[current_map + " space"].size.x / x)
+    file = open("maps/" + current_map + ".txt", "w+")
+    file.write(str(x) + "\n")
+    file.write(str(y) + "\n")
+    file.write(str(tile) + "\n")
+    file.close()
+    clear_map()
+
+def load_map(name):
+    file_path = "maps/" + name + ".txt"
+    if os.path.exists(file_path):
+        file = open("maps/" + name + ".txt", "r")
+        x = int(file.readline())
+        y = int(file.readline())
+        tile = int(file.readline())
+        make_map(x, y, tile, name)
+    else:
+        print("does not exist")
+
+def clear_map():
+    global current_map
+    current_map = None
+    my_game.maps = {}
+    my_game.spaces = {}
+    my_game.screen.window.get("DM grid").remove_grid()
+    my_game.screen.window.get("DM space").remove_space()
+
+def make_map(x, y, tile, name):
+    global current_map
+    x_space_per_pixel = x * tile / display.x
+    y_space_per_pixel = y * tile / display.y
+    space_per_pixel = max(x_space_per_pixel, y_space_per_pixel)
+    my_game.add_map([x,y], name + " map")
+    my_game.add_space([x*tile, y*tile], name + " space")
+    my_game.maps[name + " map"].fill_chessboard_pattern()
+    my_game.screen.window.get("DM grid").add_grid(my_game.maps[name + " map"].tiles)
+    my_game.screen.window.get("DM space").add_space(my_game.spaces[name + " space"], space_per_pixel)
+    current_map = name
+
+def make_new_map_inputs():
+    my_game.screen.window.get("middle bar").add_component([0, 0], [button.x, button.y / 4], 1, "map x size", background = "white", input = True)
+    my_game.screen.window.get("middle bar").add_component([0, button.y / 4], [button.x, button.y / 4], 1, "map y size", background = "white", input = True)
+    my_game.screen.window.get("middle bar").add_component([0, button.y * 2 / 4], [button.x, button.y / 4], 1, "map tile size", background = "white", input = True)
+    my_game.screen.window.get("middle bar").add_component([0, button.y * 3 / 4], [button.x, button.y / 4], 1, "map name", background = "white", input = True)
+    my_game.screen.window.get("middle bar").get("map x size").change_colours(active = "light grey")
+    my_game.screen.window.get("middle bar").get("map y size").change_colours(active = "light grey")
+    my_game.screen.window.get("middle bar").get("map tile size").change_colours(active = "light grey")
+    my_game.screen.window.get("middle bar").get("map name").change_colours(active = "light grey")
+    my_game.screen.window.get("middle bar").get("map x size").change_default_text("x size")
+    my_game.screen.window.get("middle bar").get("map y size").change_default_text("y size")
+    my_game.screen.window.get("middle bar").get("map tile size").change_default_text("tile size")
+    my_game.screen.window.get("middle bar").get("map name").change_default_text("name")
+
+def remove_new_map_inputs():
+    my_game.screen.window.get("middle bar").remove("map x size")
+    my_game.screen.window.get("middle bar").remove("map y size")
+    my_game.screen.window.get("middle bar").remove("map tile size")
+    my_game.screen.window.get("middle bar").remove("map name")
+
+def check_new_map_inputs():
+    if not my_game.output:
+        return
+    x, y, tile, name = None, None, None, None
+    for key, value in my_game.output.items():
+        if key == "map x size":
+            x = int(value)
+        if key == "map y size":
+            y = int(value)
+        if key == "map tile size":
+            tile = int(value)
+        if key == "map name":
+            name = value
+    if x and y and tile and name:
+        make_map(x, y, tile, name)
+    remove_new_map_inputs()
+
+def make_load_map_inputs():
+    my_game.screen.window.get("middle bar").add_component([0, button.y * 2], button, 1, "load map name", background = "white", input = True)
+    my_game.screen.window.get("middle bar").get("load map name").change_colours(active = "light grey")
+    my_game.screen.window.get("middle bar").get("load map name").change_default_text("name")
+
+def remove_load_map_inputs():
+    my_game.screen.window.get("middle bar").remove("load map name")
+
+def check_load_map_inputs():
+    if not my_game.output:
+        return
+    name = None
+    for key, value in my_game.output.items():
+        if key == "load map name":
+            name = value
+    if name:
+        load_map(name)
+    remove_load_map_inputs()
 
 main()
